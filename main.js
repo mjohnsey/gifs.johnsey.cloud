2bebc22baf38904e1e4e7587df316b3750625fd3
Michael Johnsey <mjohnsey@gmail.com>
2019-11-20 07:28:39 -0600

    Order by filename

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+_.mixin({
+  'sortKeysBy': function (obj, comparator) {
+      var keys = _.sortBy(_.keys(obj), function (key) {
+          return comparator ? comparator(obj[key], key) : key;
+      });
+  
+      return _.zipObject(keys, _.map(keys, function (key) {
+          return obj[key];
+      }));
+  }
+});
+
 const app = new Vue({
   el: '#gif-links',
   data: {
@@ -7,6 +19,12 @@ const app = new Vue({
     async fetchInventory() {
       const resp = await fetch('./inventory.json')
       return await resp.json()
+    }
+  },
+  computed:{
+    orderedInventory() {
+      const res = _.sortKeysBy(this.inventory.gifs);
+      return res;
     }
   },
   created() {
